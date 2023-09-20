2ab5111d500f163e186a00e7d6651b8a6d750e54
ShaniceKim <qxzo1@naver.com>
2023-09-20 16:19:12 +0900

    로그인 및 회원가입 픽스

--- app-server/src/main/java/bitcamp/myapp/controller/PointController.java
+++ app-server/src/main/java/bitcamp/myapp/controller/PointController.java
@@ -1,7 +1,7 @@
 package bitcamp.myapp.controller;
 
-import bitcamp.myapp.service.MemberService;
-import bitcamp.myapp.vo.Member;
+import bitcamp.myapp.service.UserService;
+import bitcamp.myapp.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,55 +14,55 @@ import java.util.Map;
 @Controller
 public class PointController {
 
-  @Autowired
-  MemberService memberService;
+    @Autowired
+    UserService userService;
 
-  @GetMapping("/chargePoint")
-  public String chargePointPage() {
-    return "points/chargePoint";
-  }
+    @GetMapping("/chargePoint")
+    public String chargePointPage() {
+        return "points/chargePoint";
+    }
 
-  @PostMapping("/updatepoint")
-  public ResponseEntity<Map<String, Object>> updatePoint(
-          @RequestParam("userNo") String userNo,
-          @RequestParam("bidAmount") int bidAmount, HttpSession session) throws Exception {
-
-
-    memberService.updateUserPoints(userNo, bidAmount);
-
-    // 세션 정보 업데이트
-    Member updatedUser = memberService.get(Integer.parseInt(userNo)); // 업데이트된 회원 정보 가져오기
-    session.setAttribute("loginUser", updatedUser); // 세션 업데이트
-
-    // 응답 데이터 생성 및 반환
-    Map<String, Object> response = new HashMap<>();
-    response.put("userNo", userNo);
-    response.put("bidAmount", bidAmount);
-
-    return ResponseEntity.ok(response);
-  }
+    @PostMapping("/updatepoint")
+    public ResponseEntity<Map<String, Object>> updatePoint(
+            @RequestParam("userNo") String userNo,
+            @RequestParam("bidAmount") int bidAmount, HttpSession session) throws Exception {
 
 
+        userService.updateUserPoints(userNo, bidAmount);
 
-  @PostMapping("/chargepoint")
-  public ResponseEntity<Map<String, Object>> chargepoint(
-          @RequestParam("userNo") String userNo,
-          @RequestParam("bidAmount") int bidAmount, HttpSession session) throws Exception {
+        // 세션 정보 업데이트
+        User updatedUser = userService.get(Integer.parseInt(userNo)); // 업데이트된 회원 정보 가져오기
+        session.setAttribute("loginUser", updatedUser); // 세션 업데이트
+
+        // 응답 데이터 생성 및 반환
+        Map<String, Object> response = new HashMap<>();
+        response.put("userNo", userNo);
+        response.put("bidAmount", bidAmount);
+
+        return ResponseEntity.ok(response);
+    }
 
 
-    memberService.chargeUserPoints(userNo, bidAmount);
 
-    // 세션 정보 업데이트
-    Member updatedUser = memberService.get(Integer.parseInt(userNo)); // 업데이트된 회원 정보 가져오기
-    session.setAttribute("loginUser", updatedUser); // 세션 업데이트
+    @PostMapping("/chargepoint")
+    public ResponseEntity<Map<String, Object>> chargepoint(
+            @RequestParam("userNo") String userNo,
+            @RequestParam("bidAmount") int bidAmount, HttpSession session) throws Exception {
 
-    // 응답 데이터 생성 및 반환
-    Map<String, Object> response = new HashMap<>();
-    response.put("userNo", userNo);
-    response.put("bidAmount", bidAmount);
 
-    return ResponseEntity.ok(response);
-  }
+        userService.chargeUserPoints(userNo, bidAmount);
+
+        // 세션 정보 업데이트
+        User updatedUser = userService.get(Integer.parseInt(userNo)); // 업데이트된 회원 정보 가져오기
+        session.setAttribute("loginUser", updatedUser); // 세션 업데이트
+
+        // 응답 데이터 생성 및 반환
+        Map<String, Object> response = new HashMap<>();
+        response.put("userNo", userNo);
+        response.put("bidAmount", bidAmount);
+
+        return ResponseEntity.ok(response);
+    }
 
 
 
